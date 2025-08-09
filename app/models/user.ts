a66659c65e806ea12e6d9aa7f2027edc95aef4af
Jessar-Dev <jesus_aranda_rdz@hotmail.com>
2025-08-08 18:44:38 -0600

    add user update

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,11 +1,19 @@
 import { DateTime } from 'luxon'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 import hash from '@adonisjs/core/services/hash'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
+
 import Role from './role.js'
-import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Profile from './profile.js'
+import UserQrCode from './user_qr_code.js'
+
+import Subscription from './subscription.js'
+
+import Otp from './otp.js'
+
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -15,30 +23,12 @@ const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
 export default class User extends compose(BaseModel, AuthFinder) {
   static table = 'user'
 
-  @column({ isPrimary: true })
-  declare id: number
-
-  @column()
-  declare roleId: number
-
-  @column()
-  declare email: string
-
-  @column()
-  declare password: string
-
-  @column()
-  declare isActive: boolean
-
-   @column()
-  declare uuid: string
-
-  @column()
-  declare fcm: string | null
-  
-
-  @column.dateTime({ autoCreate: true })
-  declare lastAccess: DateTime
+  @column({ isPrimary: true }) declare id: number
+  @column() declare roleId: number
+  @column() declare email: string
+  @column() declare password: string
+  @column() declare isActive: boolean
+  @column.dateTime({ autoCreate: true }) declare lastAccess: DateTime
 
   static refreshTokens = DbAccessTokensProvider.forModel(User, {
     prefix: 'rt_',
@@ -47,7 +37,10 @@ export default class User extends compose(BaseModel, AuthFinder) {
     tokenSecretLength: 40,
   })
 
-  @belongsTo(() => Role)
-public role!: BelongsTo<typeof Role>
+  @belongsTo(() => Role) declare role: BelongsTo<typeof Role>
+  @hasMany(() => Profile) declare profiles: HasMany<typeof Profile>
+  @hasMany(() => UserQrCode) declare qrCodes: HasMany<typeof UserQrCode>
+@hasMany(() => Subscription) declare subscriptions: HasMany<typeof Subscription>
+@hasMany(() => Otp) declare otps: HasMany<typeof Otp>
 
 }
